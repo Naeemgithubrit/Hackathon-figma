b135e2ebd6c111a6de8286b5540dd03325dab58e
Naeem shah <161970226+Naeemgithubrit@users.noreply.github.com>
2024-12-20 23:23:01 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,12 @@ import ContactForm from "@/components/Contactform";
 import ContactPage from "@/components/ContactDetail";
 import ContactTalkPage from "@/components/talkpage";
 import Footer from "@/components/Footer";
+import AboutHeader from "@/components/AboutHeader";
 
 export default function Contact(){
     return(
         <div>
+            <AboutHeader/>
             <ContactForm />
             <ContactPage />
             <ContactTalkPage />
