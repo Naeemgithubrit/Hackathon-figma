efa5bb254ebc84a062eace31a5e89120201d933a
Naeem shah <161970226+Naeemgithubrit@users.noreply.github.com>
2024-12-25 23:13:03 +0500

    Update AboutHeader.tsx

--- components/AboutHeader.tsx
+++ components/AboutHeader.tsx
@@ -35,23 +35,40 @@ export default function Header() {
               Contact
             </Link>
           </div>
-          <div className="relative flex justify-center items-center gap-8">
+           <div className="relative flex justify-center items-center gap-8">
             <div className="hidden lg:flex justify-center items-center">
-              <p className="text-[#23A6F0] font-montserrat text-sm font-bold mr-8">
+            <Link href="/loginPage">
+            <button className=" hover:bg-black text-blue-400 font-bold py-2 px-4 rounded">
                 Login
-              </p>
-              <button className="flex justify-center items-center gap-2 font-montserrat w-[214px] h-[52px] bg-[#23A6F0] text-white text-sm font-bold rounded-sm">
-                Become a member
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="15px"
-                  viewBox="0 -960 960 960"
-                  width="15px"
-                  fill="#ffffff"
-                >
-                  <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
-                </svg>
+                
+
               </button>
+
+            
+            </Link>
+              <div>
+           <Link href={"/teamPage"}>
+              
+              
+           <button className="flex justify-center items-center gap-2 font-montserrat w-[214px] h-[52px] bg-[#23A6F0] text-white text-sm font-bold rounded-sm">
+
+            
+
+Become a member
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="15px"
+  viewBox="0 -960 960 960"
+  width="15px"
+  fill="#ffffff"
+>
+  <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+</svg>
+
+</button>
+</Link>
+
+
             </div>
             </div>
             </div>
